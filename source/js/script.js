9b2c36e5f83dd292eaa8dd125319e884dd909ad1
geroneia <kozetta2004@yandex.ru>
2020-03-26 16:13:17 +0300

    Добавляет стилизацию мобильной версии хедера. Описывает открытие и закрытие по щелчку на кнопку

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,48 @@
 'use strict';
+var DESKTOP_WIDTH = 1024;
 var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var pageHeaderSearch = pageHeader.querySelector('.page-header__search');
+var searchInput = pageHeader.querySelector('.page-header__search-input');
+var userNavigation = pageHeader.querySelector('.page-header__user-navigation');
+var mainNav = document.querySelector('.main-nav');
+var mainNavToggle = mainNav.querySelector('.main-nav__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+mainNav.classList.remove('main-nav--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+var openMenu = function () {
+  pageHeader.classList.remove('page-header--menu-closed');
+  pageHeaderSearch.classList.remove('page-header__search--closed');
+  searchInput.classList.remove('page-header__search-input--menu-closed');
+  userNavigation.classList.remove('user-navigation--menu-closed');
+  mainNav.classList.remove('main-nav--closed');
+
+  pageHeader.classList.add('page-header--menu-opened');
+  pageHeaderSearch.classList.add('page-header__search--opened');
+  searchInput.classList.add('page-header__search-input--menu-opened');
+  userNavigation.classList.add('user-navigation--menu-opened');
+  mainNav.classList.add('main-nav--opened');
+};
+
+var closeMenu = function () {
+  pageHeader.classList.remove('page-header--menu-opened');
+  pageHeaderSearch.classList.remove('page-header__search--opened');
+  searchInput.classList.remove('page-header__search-input--menu-opened');
+  userNavigation.classList.remove('user-navigation--menu-opened');
+  mainNav.classList.remove('main-nav--opened');
+
+  pageHeader.classList.add('page-header--menu-closed');
+  pageHeaderSearch.classList.add('page-header__search--closed');
+  searchInput.classList.add('page-header__search-input--menu-closed');
+  userNavigation.classList.add('user-navigation--menu-closed');
+  mainNav.classList.add('main-nav--closed');
+};
+
+if (document.body.clientWidth <= DESKTOP_WIDTH) {
+  mainNavToggle.addEventListener('click', function () {
+    if (pageHeader.classList.contains('page-header--menu-closed')) {
+      openMenu();
+    } else {
+      closeMenu();
+    }
+  });
+}
